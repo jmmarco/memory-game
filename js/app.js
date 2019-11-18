fda07081dec0c8c27cf7eb641377ce285e9cd049
Juan Marco <jmmarco@gmail.com>
2019-11-18 09:47:41 -0300

    Rebuild icon list and handle card flip logic

--- js/app.js
+++ js/app.js
@@ -1,28 +1,70 @@
 (() => {
 
   const deck = document.getElementById('deck')
-  const icons = ['person', 'build', 'android', 'face', 'directions_car', 'emoji_events', 'fitness_center', 'flight']
+  const icons = ['fa-apple', 'fa-aviato', 'fa-aws', 'fa-android', 'fa-react', 'fa-angular', 'fa-vuejs', 'fa-airbnb']
+
+  let pairOfcards = []
 
   // Create elements
 
   const li = document.createElement('li')
   const i = document.createElement('i')
-  i.classList.add('material-icons')
+  const div = document.createElement('div')
+
 
   icons.concat(icons).forEach(icon => {
     let li = document.createElement('li')
     li.classList.add('card')
+    document.createElement('div')
     let i = document.createElement('i')
-    i.classList.add('material-icons', 'md-60')
-    console.log(i)
-    console.log(icon)
-    console.log(li)
-    i.innerText = icon
+    i.classList.add('fab')
+    i.classList.add(icon)
+    // li.classList.add('hide')
     li.appendChild(i)
-
     deck.appendChild(li)
+    li.addEventListener('click', handleClick)
   })
 
-  console.log('fired')
+
+  function checkPair(cards) {
+    if (cards[0].firstChild.classList.value === cards[1].firstChild.classList.value) {
+      cards[0].classList.add('match')
+      cards[1].classList.add('match')
+    } else {
+      setTimeout(() => {
+        cards[0].classList.remove('show', 'open')
+        cards[1].classList.remove('show', 'open')
+      }, 500)
+    }
+
+  }
+
+  function handleClick(e) {
+    let card = e.target
+
+    console.log(card)
+    console.log('pair', pairOfcards.length)
+
+    console.log(!card.classList.contains('show'))
+
+    if (pairOfcards.length < 2 && !card.classList.contains('show') ) {
+      card.classList.add('open', 'show')
+      pairOfcards.push(card)
+    }
+    
+
+    if (pairOfcards.length == 2) {
+      console.log(pairOfcards[0], pairOfcards[1])
+      checkPair(pairOfcards)
+      pairOfcards = []
+    }
+
+    // setTimeout(() => {
+    //   card.classList.remove('open', 'show')
+    // }, 500)
+    // card.classList.add('animated', 'flipInX')
+
+  }
+
 
 })()
