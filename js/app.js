baa05979e7330160790dee29950f6bd8dfcf3582
Juan Marco <jmmarco@gmail.com>
2019-11-19 15:52:56 -0300

    Add rating and move logic

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 (() => {
 
+  'use strict'
+
   const deck = document.getElementById('deck')
   const icons = ['fa-apple', 'fa-aviato', 'fa-aws', 'fa-android', 'fa-react', 'fa-angular', 'fa-vuejs', 'fa-airbnb']
-
+  const stopWatchEl = document.getElementById('stopwatch')
+  const movesEl = document.getElementById('moves')
+  let starIcons = document.getElementsByClassName('fa-star')
   let pairOfcards = []
 
   // Create elements
@@ -11,10 +15,14 @@
   const i = document.createElement('i')
   const div = document.createElement('div')
 
-  const shuffledIcons = shuffle(icons)
+  const shuffledIcons = shuffle(icons.concat(icons))
+  
+  let timerId = null
+  let pairs = icons.length
+  let moves = 0
 
 
-  shuffledIcons.concat(shuffledIcons).forEach(icon => {
+  shuffledIcons.forEach(icon => {
     let li = document.createElement('li')
     li.classList.add('card')
     document.createElement('div')
@@ -32,67 +40,140 @@
     if (cards[0].firstChild.classList.value === cards[1].firstChild.classList.value) {
       cards[0].classList.add('match')
       cards[1].classList.add('match')
+      return true
     } else {
       setTimeout(() => {
         cards[0].classList.remove('show', 'open')
         cards[1].classList.remove('show', 'open')
       }, 500)
+      return false
     }
 
   }
 
   // Non-mutating Fisher-Yates Algorithm Shuffle https://bost.ocks.org/mike/shuffle/
   function shuffle(array) {
-    const shuffledArray = [...array]
-    let m = shuffledArray.length, t, i
+    const arrayToShuffle = [...array]
+    let m = arrayToShuffle.length, t, i
     while (m) {
       i = Math.floor(Math.random() * m--)
-      t = shuffledArray[m];
-      shuffledArray[m] = shuffledArray[i];
-      shuffledArray[i] = t;
+      t = arrayToShuffle[m];
+      arrayToShuffle[m] = arrayToShuffle[i];
+      arrayToShuffle[i] = t;
     }
 
-    return shuffledArray
+    return arrayToShuffle
   }
 
-  // function shuffleArray(array) {
-  //   const shuffledArray = array.slice()
-  //   for (let i = shuffledArray.length - 1; i > 0; i--) {
-  //       const j = Math.floor(Math.random() * (i + 1));
-  //       [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  //   }
-  //   return shuffledArray
-  // }
 
-  // console.log(shuffleArray(icons))
+  function stopWatch(command, stopWatchEl) {
+
+    /* 
+      command: 'on' or 'off'
+      stopWatchEl: the destination HTML node
+    */
+
+    let clock = {
+      h: 0, m: 0, s: 0, display: null
+    }
+
+    function makeTwoDigits(number) {
+      return number < 10 ? `0${number}` : number
+    }
+
+
+    if (command) {
+      console.info('Stopwatch on.')
+      timerId = setInterval(() => {
+        clock.s++
+        if (clock.s == 60) {
+          clock.s = 0
+          clock.m++
+        }
+
+        if (clock.m == 60) {
+          clock.m = 0
+          clock.h++
+        }
+
+        if (clock.h == 24) {
+          clock.h = 0
+        }
+
+        clock.display = `${makeTwoDigits(clock.h)}:${makeTwoDigits(clock.m)}:${makeTwoDigits(clock.s)}`
+        stopWatchEl.innerText = clock.display
+      }, 1000)
+    } else if (!command) {
+      console.info('Stopwatch off.');
+      clearInterval(timerId);
+    }
+
+    return timerId
+  }
 
 
 
   function handleClick(e) {
+
+    console.log('target is: ', e.target)
+
     let card = e.target
 
-    console.log(card)
-    console.log('pair', pairOfcards.length)
 
-    console.log(!card.classList.contains('show'))
+    if (!timerId) {
+      timerId = stopWatch(true, stopWatchEl)
+    }
 
-    if (pairOfcards.length < 2 && !card.classList.contains('show') ) {
-      card.classList.add('open', 'show')
+    if (!card.classList.contains('show') && card.classList.contains('card') && pairOfcards.length < 2) {
+      card.classList.add('show', 'open')
+      console.log('card now has', card)
       pairOfcards.push(card)
     }
-    
 
-    if (pairOfcards.length == 2) {
-      console.log(pairOfcards[0], pairOfcards[1])
-      checkPair(pairOfcards)
+    if (pairOfcards.length === 2) {
+      countMatch(checkPair(pairOfcards))
       pairOfcards = []
     }
 
-    // setTimeout(() => {
-    //   card.classList.remove('open', 'show')
-    // }, 500)
-    // card.classList.add('animated', 'flipInX')
+    checkWin(pairs)
 
+  }
+
+
+  function countMatch(outcome) {
+    console.log('inside countMatch', outcome)
+    moves++
+    if (outcome) {
+      pairs--
+    }
+    updateMoves(moves, movesEl)
+    setRating(starIcons, moves)
+  }
+
+  function checkWin(pairs) {
+    if (pairs === 0) {
+      alert('you\'ve won')
+      stopWatch(false, stopWatchEl)
+    }
+  }
+
+  function updateMoves(moves, target) {
+    target.innerText = moves
+  }
+
+  function setRating(stars, moves) {
+    switch(moves) {
+      case 10:
+        stars[2].classList.remove('fas')
+        stars[2].classList.add('far')
+        break
+      case 20:
+        stars[1].classList.remove('fas')
+        stars[1].classList.add('far')
+        break
+      default:
+        break
+    }
   }
 
 
